ec806463c71200fa5da3a824b75c9d62072a778b
aditeddirevanth <aditeddirevanth@gmail.com>
2026-03-03 14:55:58 +0530

    db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -19,6 +19,3 @@ $conn = mysqli_connect($host, $user, $pass, $db, $port);
 if (!$conn) {
     die("Database Connection Failed: " . mysqli_connect_error());
 }
-
-echo "Database Connected Successfully!";
-?>
