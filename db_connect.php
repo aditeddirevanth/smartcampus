485837398c00ebbe7b56ef0bff753fbafd5971dd
aditeddirevanth <aditeddirevanth@gmail.com>
2026-03-03 14:44:17 +0530

    db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -1,14 +1,16 @@
 <?php
 
-$host = getenv("MYSQLHOST");
-$user = getenv("MYSQLUSER");
-$pass = getenv("MYSQLPASSWORD");
-$db   = getenv("MYSQLDATABASE");
-$port = (int) getenv("MYSQLPORT");  // Convert to integer
+$host = $_ENV['MYSQLHOST'] ?? $_SERVER['MYSQLHOST'];
+$user = $_ENV['MYSQLUSER'] ?? $_SERVER['MYSQLUSER'];
+$pass = $_ENV['MYSQLPASSWORD'] ?? $_SERVER['MYSQLPASSWORD'];
+$db   = $_ENV['MYSQLDATABASE'] ?? $_SERVER['MYSQLDATABASE'];
+$port = (int) ($_ENV['MYSQLPORT'] ?? $_SERVER['MYSQLPORT']);
 
 $conn = mysqli_connect($host, $user, $pass, $db, $port);
 
 if (!$conn) {
     die("Database Connection Failed: " . mysqli_connect_error());
 }
+
+echo "Database Connected Successfully!";
 ?>
